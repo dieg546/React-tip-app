84e10f06f9be871419897d2f838a91008c30bdd6
Diego Sanchez <disaru2000@gmail.com>
2025-11-26 07:37:04 -0400

    segundo commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <header className=' p-5 bg-teal-500'>
+        <h1 className=' text-2xl font-bold text-center'>Calculadora de Propinas Easy</h1>
+      </header>
+
+      {/* <h1 className=" text-2xl font-bold">Hola mundito</h1> */}
+    </>
+  )
+}
+
+export default App
